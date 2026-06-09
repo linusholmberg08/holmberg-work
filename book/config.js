f53afc944ce1dde4638e3b9488290fe120261e2d
Linus Holmberg <240869467+linusholmberg08@users.noreply.github.com>
2026-06-09 10:08:29 +0200

    Simplify declutter site and add book page

---
+++ book/config.js
@@ -0,0 +1,3 @@
+// Add the live Stripe Payment Link here when the e-book product is created.
+// Example: window.TDL_BOOK_PURCHASE_URL = "https://buy.stripe.com/...";
+window.TDL_BOOK_PURCHASE_URL = "";
